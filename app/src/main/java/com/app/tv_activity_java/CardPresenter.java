f0d839836214ccb99f630aac82b12a37f0d5dc3d
Wyatt Evans <syattevans@gmail.com>
2020-08-15 18:38:24 -0600

    Cleaned up CardPresenter.

--- app/src/main/java/com/app/tv_activity_java/CardPresenter.java
+++ app/src/main/java/com/app/tv_activity_java/CardPresenter.java
@@ -25,10 +25,6 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 
-/*
- * A CardPresenter is used to generate Views and bind Objects to them on demand.
- * It contains an Image CardView
- */
 public class CardPresenter extends Presenter {
     private static final String TAG = "CardPresenter";
 
@@ -40,8 +36,7 @@ public class CardPresenter extends Presenter {
 
     private static void updateCardBackgroundColor(ImageCardView view, boolean selected) {
         int color = selected ? sSelectedBackgroundColor : sDefaultBackgroundColor;
-        // Both background colors should be set because the view's background is temporarily visible
-        // during animations.
+
         view.setBackgroundColor(color);
         view.findViewById(R.id.info_field).setBackgroundColor(color);
     }
@@ -54,11 +49,7 @@ public class CardPresenter extends Presenter {
                 ContextCompat.getColor(parent.getContext(), R.color.default_background);
         sSelectedBackgroundColor =
                 ContextCompat.getColor(parent.getContext(), R.color.selected_background);
-        /*
-         * This template uses a default image in res/drawable, but the general case for Android TV
-         * will require your resources in xhdpi. For more information, see
-         * https://developer.android.com/training/tv/start/layouts.html#density-resources
-         */
+
         mDefaultCardImage = ContextCompat.getDrawable(parent.getContext(), R.drawable.movie);
 
         ImageCardView cardView =
@@ -98,7 +89,6 @@ public class CardPresenter extends Presenter {
     public void onUnbindViewHolder(Presenter.ViewHolder viewHolder) {
         Log.d(TAG, "onUnbindViewHolder");
         ImageCardView cardView = (ImageCardView) viewHolder.view;
-        // Remove references to images so that the garbage collector can free up memory
         cardView.setBadgeImage(null);
         cardView.setMainImage(null);
     }
